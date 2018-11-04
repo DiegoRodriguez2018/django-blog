9d5995287dc852dbdf3eeb5d75a16dd0c9ae11fe
Diego Rodriguez <diego.f.rodriguezh@gmail.com>
2018-11-04 16:44:24 +1100

    First commit, added a blog app and a basic model

---
+++ blog/models.py
@@ -0,0 +1,22 @@
+from django.db import models
+from django.core.urlresolvers import reverse 
+
+# Create your models here.
+
+#Note that Post is inheriting from models.Model
+class Post(models.Model):
+    title =models.CharField(max_length=255)
+    slug = models.SlugField(max_length=255, unique=True)
+    summary = models.CharField(max_length=300)
+    content = models.TextField()
+    published = models.BooleanField(default=True)
+    created = models.DateTimeField(auto_now_add=True)
+
+#we add a subclas called Meta
+    class Meta:
+        ordering = ['-created']
+    def __unicode__(self):
+        return u'%s'% self.title
+    def get_absolute_url(self):
+        return reverse('blog.view.post', args=[self.slug])
+        
