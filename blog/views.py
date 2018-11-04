b5a2ed84deba81bb05a0305b7dacbc174ae619ff
Diego Rodriguez <diego.f.rodriguezh@gmail.com>
2018-11-04 17:50:32 +1100

    Implemented a simple template for index

--- blog/views.py
+++ blog/views.py
@@ -4,7 +4,9 @@ from django.http import HttpResponse
 
 # We create our views here as methods, and when we create views in django we always pass an request argument.
 def index(request):
-    return HttpResponse("Hey there.")
+    # return HttpResponse("Hey there.")
+    return render (request, 'index.html',{})
+    # Notice that render takes a request, the html file, and parameters. In this case is an empty dictionary but we can pass values as well. 
 
 def post(request):
     return HttpResponse("I'm a single post page.")
