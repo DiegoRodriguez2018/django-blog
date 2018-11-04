aa3d5895b1f61c56e0b4d5229925adfe1dab7c23
Diego Rodriguez <diego.f.rodriguezh@gmail.com>
2018-11-04 17:40:14 +1100

    implemented index and post views, and specified the corresponing urls

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
-
+from django.http import HttpResponse
 # Create your views here.
+
+# We create our views here as methods, and when we create views in django we always pass an request argument.
+def index(request):
+    return HttpResponse("Hey there.")
+
+def post(request):
+    return HttpResponse("I'm a single post page.")
