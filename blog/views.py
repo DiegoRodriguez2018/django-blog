7d976490702d353e8955a54b578694ae1cb0aab3
Diego Rodriguez <diego.f.rodriguezh@gmail.com>
2018-11-06 11:34:47 +1100

    Fully implemented index post and about templates

--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, render_to_response, get_object_or_404
 from django.http import HttpResponse
 from .models import Post
 # Create your views here.
@@ -11,5 +11,10 @@ def index(request):
     return render (request, 'index.html',{'posts':posts})
     # Notice that we are passing parameters to our template in the dictionary. 
 
-def post(request):
-    return HttpResponse("I'm a single post page.")
+def post(request,slug):
+    return render_to_response('post.html', {
+        'post': get_object_or_404(Post,slug=slug) 
+    })
+
+def about(request):
+    return render (request, 'about.html')
